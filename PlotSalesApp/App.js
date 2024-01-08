6cb0b112b2c9edb3199bec1cde1c6e4ddbec8b9d
Malik Umar <umargulzar2001@gmail.com>
2024-01-08 22:14:56 +0500

    finally cheking both web and adroid plateforms for category and payment method and styed correctly

--- PlotSalesApp/App.js
+++ PlotSalesApp/App.js
@@ -1,23 +1,37 @@
 import React from "react";
-import { StyleSheet, StatusBar } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
+import { StatusBar, StyleSheet, Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import store from "./src/redux/store/store";
 import Navigation from "./Navigation";
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Provider store={store}>
-        <StatusBar translucent backgroundColor="green" />
-        <Navigation />
-      </Provider>
-    </SafeAreaView>
+    <React.Fragment>
+      {Platform.OS !== "web" && (
+        <SafeAreaView style={styles.container}>
+          <StatusBar translucent backgroundColor="green" />
+          <Provider store={store}>
+            <Navigation />
+          </Provider>
+        </SafeAreaView>
+      )}
+      {Platform.OS === "web" && (
+        <Provider store={store}>
+          <StatusBar style={styles.webStatusBar} />
+          <Navigation />
+        </Provider>
+      )}
+    </React.Fragment>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  webStatusBar: {
+    backgroundColor: "green",
   },
 });
 
