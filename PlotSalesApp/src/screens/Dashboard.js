e846bab742e4a6edec362a4959d0b36a619d0322
Malik Umar <umargulzar2001@gmail.com>
2024-01-11 01:29:52 +0500

    moved from local databse to mongodb atlas (copying the string for node version 2.2.12 or later

--- PlotSalesApp/src/screens/Dashboard.js
+++ PlotSalesApp/src/screens/Dashboard.js
@@ -1,22 +1,27 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import React from "react";
 import dashboardStyles from "../styles/dashboardStyles";
 import DashboardLinks from "./forms/components/DashboardLinks";
 
 const Dashboard = () => {
   return (
-    <View style={dashboardStyles.dashboard}>
-      <DashboardLinks title={`Add Reservation`} link={`ReservationForm`} />
-      <DashboardLinks title={`Add Plot Booking`} link={`BookingForm`} />
-      <DashboardLinks title={`Add Town Planning`} link={`TownPlanningForm`} />
-      <DashboardLinks title={`Add Plot Category`} link={`PlotCategoryForm`} />
-      <DashboardLinks title={`Add Payment Method`} link={`PaymentMethodForm`} />
-      <DashboardLinks title={`Town Planning`} link={`TownPlanning`} />
-      <DashboardLinks title={`Reservation`} link={`Reservation`} />
-      <DashboardLinks title={`Plot Booking`} link={`Booking`} />
-      <DashboardLinks title={`Category`} link={`Category`} />
-      <DashboardLinks title={`Payment Methods`} link={`PaymentMethods`} />
-    </View>
+    <ScrollView>
+      <View style={dashboardStyles.dashboard}>
+        <DashboardLinks title={`Add Reservation`} link={`ReservationForm`} />
+        <DashboardLinks title={`Add Plot Booking`} link={`BookingForm`} />
+        <DashboardLinks title={`Add Town Planning`} link={`TownPlanningForm`} />
+        <DashboardLinks title={`Add Plot Category`} link={`PlotCategoryForm`} />
+        <DashboardLinks
+          title={`Add Payment Method`}
+          link={`PaymentMethodForm`}
+        />
+        <DashboardLinks title={`Town Planning`} link={`TownPlanning`} />
+        <DashboardLinks title={`Reservation`} link={`Reservation`} />
+        <DashboardLinks title={`Plot Booking`} link={`Booking`} />
+        <DashboardLinks title={`Category`} link={`Category`} />
+        <DashboardLinks title={`Payment Methods`} link={`PaymentMethods`} />
+      </View>
+    </ScrollView>
   );
 };
 
